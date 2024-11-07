98acd58e7fc357a8a1c0cb0e44d49e706899fc50
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-11-07 11:27:23 +0900

    Add test

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 
 ng () {
 	echo ${1}行目が違うよ
@@ -7,7 +7,18 @@ ng () {
 
 res=0
 
+### 変な入力 ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "$out" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "$out" = "" ] || ng "$LINENO"
+
+### 正常な入力 ###
 out=$(seq 5 | ./plus)
+[ "$?" = 0 ] || ng "$LINENO"
 [ "$out" = 15 ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
